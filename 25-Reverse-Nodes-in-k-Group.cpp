ee637dacd35c12bf821ba4d47389d41953c2a66f
James Upson <112772819+WhiteDeaths@users.noreply.github.com>
2024-10-23 12:04:41 +0100

    [25] [Time Beats: 100.00%] [Memory Beats: 55.89%] - LeetPush

---
+++ 25-Reverse-Nodes-in-k-Group.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode dummy{0};
+        dummy.next = head;
+        auto curr = head, curr_dummy = &dummy;
+        int len = 0;
+
+        while (curr) {
+            auto next_curr = curr->next;
+            len = (len + 1) % k;
+
+            if (len == 0) {
+                auto next_dummy = curr_dummy->next;
+                reverse(&curr_dummy, curr->next);
+                curr_dummy = next_dummy;
+            }
+            curr = next_curr;
+        }
+        return dummy.next;
+    }
+
+    void reverse(ListNode **begin, const ListNode *end) {
+        ListNode *first = (*begin)->next;
+        ListNode *curr = first->next;
+
+        while (curr != end) {
+            first->next = curr->next;
+            curr->next = (*begin)->next;
+            (*begin)->next = curr;
+            curr = first->next;
+        }
+    }
+};
